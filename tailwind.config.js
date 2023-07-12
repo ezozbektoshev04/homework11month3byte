37c85824682279b1b170b7e8561ff6313b6256c7
ezozbektoshev04 <toshevezozbek04@gmail.com>
2023-07-13 04:09:46 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1280px",
+      xxl: "1320px",
+    },
+    extend: {
+      colors: {
+        "header-color": "#1C1E53",
+        "text-color": "#BBBBCB",
+        "nav-button-border": "rgba(244, 246, 252, 0.20)",
+        "first-color": "#282938",
+        "second-color": "#F4F5F5",
+        "third-color": "#F4F6FC",
+        "fourth-color": "#1D2130",
+        "fifeth-color": "#FCD980",
+        "sixth-color": "#2405F2",
+        "seventh-color": "rgba(0, 0, 0, 0.12)",
+      },
+    },
+  },
+  plugins: [],
+};
